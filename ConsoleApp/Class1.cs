acf36826a0889adf679ba70bd86cc26dddbd2a22
Colt Bauman <colt.bauman@unity3d.com>
2023-04-12 20:09:24 +0900

    Add project files.

---
+++ ConsoleApp/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+var class1 = new Class1();
+class1.Method1();
